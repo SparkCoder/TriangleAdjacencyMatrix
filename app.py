38c5c557e228395a13097e8a7a5c569f9b4d45ab
SparkCoder <abhishekdinesh3@gmail.com>
2021-05-07 19:52:49 +0530

    Added add label feature

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from PySide2.QtUiTools import QUiLoader
-from PySide2.QtWidgets import QApplication, QPushButton
+from PySide2.QtWidgets import QApplication, QWidget, QPushButton, QLabel, QHBoxLayout, QListWidgetItem
 from PySide2.QtCore import QFile, QObject
+from PySide2.QtCore import Qt as Qt
 from PySide2.QtGui import QIcon
 
 import numpy as np
@@ -13,6 +14,23 @@ from lib.ui import res_rc
 
 # Globals
 app_root = os.path.dirname(os.path.realpath(__file__))
+
+
+class LabelListItem(QWidget):
+    def __init__(self, name, parent=None):
+        super(LabelListItem, self).__init__(parent)
+
+        self.label = QLabel(name)
+        self.button = QPushButton("DELETE")
+
+        self.row = QHBoxLayout(self)
+
+        self.row.addWidget(self.label)
+        self.row.addWidget(self.button)
+
+        self.row.setContentsMargins(0, 0, 0, 0)
+
+        self.setLayout(self.row)
 
 
 class App(QObject):
@@ -40,12 +58,29 @@ class App(QObject):
             'item 3'
         ]
         self.__generate_matrix()
-
         self.__load_ui(ui_file_name, title)
 
-        # Events
-
         self.__update_matrix()
+
+        self.window.labels.clear()
+        for label in self.labels:
+            self.__add_label(label, list_update=False)
+
+        # Events
+        self.window.addLabelBtn.clicked.connect(self.add_label_event)
+
+    def add_label_event(self):
+        self.__add_label('item ' + str(len(self.labels) + 1))
+        self.__generate_matrix()
+        self.__update_matrix()
+
+    def __add_label(self, label, list_update=True):
+        if list_update:
+            self.labels.append(label)
+        row = LabelListItem(label)
+        item = QListWidgetItem(self.window.labels)
+        self.window.labels.addItem(item)
+        self.window.labels.setItemWidget(item, row)
 
     def __clearLayout(self, layout):
         for i in reversed(range(layout.count())):
@@ -67,14 +102,25 @@ class App(QObject):
         label_count = len(self.labels)
         self.__clearLayout(self.window.matrix)
         self.window.matrix.cellRect(label_count, label_count)
+        self.window.matrix.setAlignment(Qt.AlignVCenter)
+
+        for col in range(label_count):
+            label = QLabel(self.labels[col])
+            label.setAlignment(Qt.AlignBottom | Qt.AlignHCenter)
+            self.window.matrix.addWidget(label, 0, col + 1)
 
         for row in range(label_count):
+            label = QLabel(self.labels[row])
+            label.setAlignment(Qt.AlignRight | Qt.AlignVCenter)
+            self.window.matrix.addWidget(label, row + 1, 0)
+
             for col in range(row + 1):
                 self.window.matrix.addWidget(
-                    self.__generate_matrix_button(self.matrix[row, col]), row, col)
+                    self.__generate_matrix_button(self.matrix[row, col]), row + 1, col + 1)
+
             for col in range(row + 1, label_count):
                 self.window.matrix.addWidget(self.__generate_matrix_button(
-                    self.matrix[row, col], disabled=True), row, col)
+                    self.matrix[row, col], disabled=True), row + 1, col + 1)
 
     def __generate_matrix_button(self, kind, disabled=False):
         button = QPushButton()
